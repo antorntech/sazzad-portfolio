69cc7e65986ce94f51b2725f5600e91928fcd6cc
MD Abdul Motaleb Antor <antor@piit.local>
2024-07-29 10:05:45 +0600

    add some change on loader

--- src/loaders/Loader.jsx
+++ src/loaders/Loader.jsx
@@ -1,27 +1,41 @@
+// import React from "react";
+// import "./Loader.css";
+
+// const Loader = () => {
+//   return (
+//     <div className="loader-main">
+//       <div className="lds-spinner">
+//         <div></div>
+//         <div></div>
+//         <div></div>
+//         <div></div>
+//         <div></div>
+//         <div></div>
+//         <div></div>
+//         <div></div>
+//         <div></div>
+//         <div></div>
+//         <div></div>
+//         <div></div>
+//       </div>
+//       <div>
+//         {/* <img src="/images/s.png" alt="" className="w-[250px]" /> */}
+//         <h1 className="text-2xl font-bold text-white">SAZZAD</h1>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Loader;
+
 import React from "react";
-import "./Loader.css";
 
 const Loader = () => {
   return (
-    <div className="loader-main">
-      <div className="lds-spinner">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-      <div>
-        {/* <img src="/images/s.png" alt="" className="w-[250px]" /> */}
-        <h1 className="text-2xl font-bold text-white">SAZZAD</h1>
-      </div>
+    <div className="bg-[#18377e] w-full h-screen flex justify-center items-center">
+      <h1 className="text-3xl uppercase font-bold text-white animate-pulse">
+        SA<span className="text-[#d64734]">ZZ</span>AD
+      </h1>
     </div>
   );
 };
